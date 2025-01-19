da89fbaadc0def1dc2ec401c86ac4d3741bf79a3
Christian Manzaraz <magnusmanz@icloud.com>
2025-01-19 13:03:11 -0300

    Initial Commit

---
+++ Elvis/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Elvis
+//
+//  Created by Christian Manzaraz on 19/01/2025.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Elvis/ElvisApp.swift
@@ -0,0 +1,17 @@
+//
+//  ElvisApp.swift
+//  Elvis
+//
+//  Created by Christian Manzaraz on 19/01/2025.
+//
+
+import SwiftUI
+
+@main
+struct ElvisApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
